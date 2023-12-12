14f2fd36252f2be678f9156699057f526284707f
pdrHBS <pedrobezerraout@gmail.com>
2023-12-12 10:20:54 -0300

    testes php

---
+++ fundamentos_basicos.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Olá mundo</h1>
+    <?php
+    echo 'Olá mundo 2 <br>'; 
+    echo 'Meu nome é Pedro <br>'; 
+    echo 'Minha idade é 23aaaa';
+    ?>
+</body>
+</html>
+
